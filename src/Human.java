93c4bb3e9e621a266a551b7579f37c0c122b6edd
Yurii Polozhevets <yi.polozhevets@gmail.com>
2022-12-07 07:22:50 +0200

    added a new class object Main

--- src/Human.java
+++ src/Human.java
@@ -1,16 +1,14 @@
 public class Human {
     String firstName;
     String lastName;
-    String phoneNumber;
     static int id;
 
     {
         id++;
     }
-    Human(String fn, String ln, String p){
+    Human(String fn, String ln){
         firstName = fn;
         lastName = ln;
-        phoneNumber = p;
     }
 
     void setFirstName(String fn){
@@ -19,20 +17,13 @@ public class Human {
     void setLastName(String ln){
         lastName = ln;
     }
-    void setPhoneNumber(String p){
-        phoneNumber = p;
-    }
-
     void getFirstName(){
         System.out.println("Name: "+firstName);
     }
     void getLastName(){
         System.out.println("Name: "+lastName);
     }
-    void getPhoneNumber(){
-        System.out.println("Phone number: "+phoneNumber);
-    }
     void showHuman(){
-        System.out.printf(" id: %d\n First name: %s \n Last name: %s \n Phone number: %s", id, firstName ,lastName, phoneNumber);
+        System.out.printf(" id: %d\n First name: %s \n Last name: %s \n", id, firstName ,lastName);
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,9 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
 
-        Human yurii = new Human("Yurii", "Polozhevets", "38068255****");
+        Human yurii = new Human("Yurii", "Polozhevets");
         yurii.showHuman();
+        Books JavaDeveloper = new Books("Java Developer", 9.2);
+        JavaDeveloper.showBooks();
     }
 }
